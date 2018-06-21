9cb8d59131cbe0b6be029866189eb5f5eb55fca0
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-21 14:25:48 -0500

    Update ScalarVecMult.py

--- ScalarVecMult.py
+++ ScalarVecMult.py
@@ -1,4 +1,8 @@
 def scalarMultVec(scalar,vector):
+  '''
+  This function takes a scalar and a vector and multiplies them together. A is an empty list. For each element in length i, multiply
+  by the scalar. This creates a new list for A and returns A.
+  '''
   A = [ ]
   for i in range(len(vector)):
     A.append(vector[i] * scalar)
