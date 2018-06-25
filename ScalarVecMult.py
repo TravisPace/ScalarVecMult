4b9a5361e743f68a9145439abfaced5a939efc05
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-24 19:41:34 -0500

    Update ScalarVecMult.py

--- ScalarVecMult.py
+++ ScalarVecMult.py
@@ -1,12 +1,16 @@
 def scalarMultVec(scalar,vector):
   '''
-  This function takes a scalar and a vector and multiplies them together. A is an empty list. For each element in length i, multiply
-  by the scalar. This creates a new list for A and returns A.
+  This function takes a scalar and a vector and multiplies them together.
   '''
   if type(scalar) == list:
     print('error')
     return None
+  #If the scalar is a list, then the algorithim will print error and return none.
   A = [ ]
+  #A is an empty list.
   for i in range(len(vector)):
+    #For the length of the vector.
     A.append(vector[i] * scalar)
+    #For each element in length i, multiply by the scalar.
   return A
+  #This creates a new list for A and returns A.
